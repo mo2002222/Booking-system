06fef2de68f3dd79761f886c1fadb18858f6fb08
mohamed Ail <159685876+mo2002222@users.noreply.github.com>
2025-09-13 15:20:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Booking-system/', // <-- Add this line (use your repo name)
+  base: '/Booking-system/', 
+  plugins: [react()],
 })
